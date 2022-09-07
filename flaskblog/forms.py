730cac19bbd9b368ac3f3da701cb65db9e57c03d
AbdulMuhaymin <abdul.muhaymin@ug.bilkent.edu.tr>
2022-09-07 07:55:58 +0300

    1

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -48,8 +48,75 @@ class UpdateAccountForm(FlaskForm):
                 raise ValidationError('That email is already in use. Please choose another one.')
 
 class SystemForm(FlaskForm):
-    name = StringField('Title', validators=[DataRequired()])
+    system_name = StringField('Title', validators=[DataRequired()])
+    gaia_id
     sp_type =  StringField('Spectral Type')
+
+    sp_type = db.Column()
+    sp_type_ref = db.Column()
+
+    ra = StringField('')
+    ra_error_upper = db.Column()
+    ra_error_lower = db.Column()
+    dec = db.Column()
+    dec_error_upper = db.Column()
+    dec_error_lower = db.Column()
+    ra_dec_ref = StringField('')
+
+    parallax = db.Column()
+    parallax_error = db.Column()
+    distance = db.Column()
+    distance_error_upper = db.Column()
+    distance_error_lower = db.Column()
+    distance_ref = StringField('')
+
+    distance_gspphot = db.Column()
+    distance_gspphot_upperper = db.Column()
+    distance_gspphot_lower = db.Column()
+
+    pmra = db.Column()
+    pmra_error_upper = db.Column()
+    pmra_error_lower = db.Column()
+    pmdec = db.Column()
+    pmdec_error_upper = db.Column()
+    pmdec_error_lower = db.Column()
+    pm = db.Column()
+    pm_ref = StringField('')
+
+    radial_velocity = db.Column()
+    radial_velocity_upper = db.Column()
+    radial_velocity_lower = db.Column()
+    radial_velocity_ref = StringField('')
+
+    a = db.Column()
+    a_error_upper = db.Column()
+    a_error_lower = db.Column()
+    a_ref = StringField('')
+
+    incl = db.Column()
+    incl_error_upper = db.Column()
+    incl_error_lower = db.Column()
+    incl_ref = StringField('')
+
+    e = db.Column()
+    e_error_upper = db.Column()
+    e_error_lower = db.Column()
+    e_ref = StringField('')
+    
+    w = db.Column()
+    w_error_upper = db.Column()
+    w_error_lower = db.Column()
+    w_ref = StringField('')
+
+    age = db.Column()
+    age_error_upper = db.Column()
+    age_error_lower = db.Column()
+    age_ref = StringField('')
+
+    alt_names = TextAreaField()
+    notes = TextAreaField()
+
+
     submit = SubmitField('Submit')
 
 class StarForm(FlaskForm):
