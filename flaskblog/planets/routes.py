08d68c8f87c798df6491f7fb43cb10c9635fc6c5
AbdulMuhaymin <abdul.muhaymin@ug.bilkent.edu.tr>
2022-09-10 04:31:22 +0300

    updating star and planet fields

--- flaskblog/planets/routes.py
+++ flaskblog/planets/routes.py
@@ -8,10 +8,18 @@ planets_bp = Blueprint('planets', __name__)
 
 @planets_bp.route("/planets")
 def planets():
-    return render_template('planets.html', title='Planets')
+    planets = Planet.query.all()
+    return render_template('planets.html', title="Planet Catalogue", planets=planets)
 
-@planets_bp.route("/planets/new")
+@planets_bp.route("/planets/new", methods=['GET', 'POST'])
 @login_required
 def new_planet():
-    return render_template('planet_new.html', title='Add a planet')
-
+    form = PlanetForm()
+    if form.validate_on_submit():
+        planet = Planet(**{k: v for k, v in form.data.items() if k not in ['csrf_token', 'submit']}, 
+            author=current_user)
+        db.session.add(planet)
+        db.session.commit()
+        flash('The planet entry has been successfully added for ', 'success')
+        return redirect(url_for('planets.planet', planet_id=planet.id))
+    return render_template('planet_new.html', title="Add new Planet", form=form, legend='Add New Planet')
